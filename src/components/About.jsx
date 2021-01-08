1d58648050275274adca441b6696cdcddc56958d
srzaz <srz2@humboldt.edu>
2021-01-08 13:42:14 -0800

    updates on about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import surreal from '../assets/surreal.jpg';
+import email from "../assets/emailicon.png";
+import github from "../assets/github_icon.png";
+import linkedin from "../assets/linkedin_icon.png";
 
 function About() {
     return (
@@ -7,18 +9,25 @@ function About() {
             <div class="container">
                 <div class="row align-items-center my-5">
                     <div class="col-lg-7">
-                        <div class="thumb">
-                            <a href="#">
-                                <img src={surreal} style={{ height: '100%', width: '100%' }} /> </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-5">
                         <h1 class="font-weight-light">About</h1>
                         <p>
                             Amateur programmer, amateur artist, and Computer Science student.
-            </p>
+                         </p>
                     </div>
                 </div>
+                <div class="col-lg-7">
+
+                    <a href="mailto: srz2@humboldt.edu">
+                        <img src={email} style={{ height: '10%', width: '10%', margin: '25px 25px 25px 25px' }} /> </a>
+
+                    <a href="https://github.com/srzaz">
+                        <img src={github} style={{ height: '10%', width: '10%', margin: '25px 25px 25px 25px' }} /> </a>
+
+                    <a href="https://www.linkedin.com/in/stephen-zazueta-a126901b8/">
+                        <img src={linkedin} style={{ height: '10%', width: '10%', margin: '25px 25px 25px 25px' }} /> </a>
+                </div>
+
+
             </div>
         </div>
     );
